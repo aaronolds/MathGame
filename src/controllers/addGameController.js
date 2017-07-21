ecca2f911bbfb469b311379647b29cc922ba3137
Aaron P. Olds <aaron.olds@gmail.com>
2017-07-20 21:57:22 -0400

    update service

--- src/controllers/addGameController.js
+++ src/controllers/addGameController.js
@@ -1,18 +1,26 @@
 exports.newGame = function (req, res) {
     // console.log(req.params.level);
     res.setHeader('Content-Type', 'application/json');
-    var arrayOfObjects = [{ "q": "1 + 0", "a": "1" },
-    { "q": "1 + 0", "a": "1" },
-    { "q": "1 + 1", "a": "2" },
-    { "q": "1 + 2", "a": "3" },
-    { "q": "1 + 3", "a": "4" },
-    { "q": "1 + 4", "a": "5" },
-    { "q": "1 + 5", "a": "6" },
-    { "q": "1 + 6", "a": "7" },
-    { "q": "1 + 7", "a": "8" },
-    { "q": "1 + 8", "a": "9" },
-    { "q": "1 + 9", "a": "10" },
-    { "q": "1 + 10", "a": "11" }];
+    // var arrayOfObjects = [{ "q": "1 + 0", "a": "1" },
+    // { "q": "1 + 0", "a": "1" },
+    // { "q": "1 + 1", "a": "2" },
+    // { "q": "1 + 2", "a": "3" },
+    // { "q": "1 + 3", "a": "4" },
+    // { "q": "1 + 4", "a": "5" },
+    // { "q": "1 + 5", "a": "6" },
+    // { "q": "1 + 6", "a": "7" },
+    // { "q": "1 + 7", "a": "8" },
+    // { "q": "1 + 8", "a": "9" },
+    // { "q": "1 + 9", "a": "10" },
+    // { "q": "1 + 10", "a": "11" }];
+    var arrayOfObjects = [];
+    for (var i = 0; i < 11; i++) {
+        var obj = {
+            q: `${req.params.level} + ${i}`,
+            a: i + parseInt(req.params.level)
+        };
+        arrayOfObjects.push(obj);
+    }
 
     res.send(JSON.stringify(arrayOfObjects, null, 3));
     // switch (req.params.level) {
